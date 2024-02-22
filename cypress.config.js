461d9dd349aff5d17293a1b27b685367bc857238
Camilla Marques dos Santos <camilla.santos@itschmitz.com>
2024-02-22 14:32:43 -0300

    refactor:ajustes

--- cypress.config.js
+++ cypress.config.js
@@ -4,17 +4,18 @@ const fs = require('fs');
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
+      allureWriter(on, config);
       on('task', {
         readDirectory: (path) => {
           return fs.readdirSync(path);
         },
       });
-      allureWriter(on, config);
-      config.specPattern = [
-       "cypress/e2e/blogdoagi/tests/test01.CT001.cy.ts",
-       "cypress/e2e/blogdoagi/tests/test02.CT002.cy.ts",
-      ];
-      return config;
+     
+      // config.specPattern = [
+      //  "cypress/e2e/blogdoagi/tests/test01.CT001.cy.ts",
+      //  "cypress/e2e/blogdoagi/tests/test02.CT002.cy.ts",
+      // ];
+      
     },
     baseUrl: 'https://blogdoagi.com.br/',
     chromeWebSecurity: false,
